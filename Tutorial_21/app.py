e634de4c6f902b8674f958a4b0f554396d1bf4b6
AdamFitzz <adammike745@gmail.com>
2025-01-29 23:37:13 +0000

    Adding tutorial_20, tutorial on jinja filters and bootstrap columns

--- Tutorial_21/app.py
+++ Tutorial_21/app.py
@@ -10,10 +10,11 @@ app = Flask(__name__)
 def hello():
     #calling render_template to display index html file
     #passing date and time in with it to be displayed by Jinja2
+    #datetime.datetime = datetime class from the datetime module
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
 #Creating a new endpoint called /about this will return a new html page
-#It will look like http://{ip address}:5000/about
+#It will look like http://{ip addrss}:5000/about
 @app.route('/about/')
 def about():
     #Calling the render_template function to return about.html page to client
@@ -23,7 +24,7 @@ def about():
 #It will look like http://{ip address}:5000/comments
 @app.route('/comments/')
 def comments():
-    # comments array hard coded but would be pulled from a database normally
+    # comments list hard coded but would be pulled from a database normally
     comments = ['This is the first comment.',
                 'This is the second comment.',
                 'This is the third comment.',
@@ -42,6 +43,11 @@ def comments2():
                 ]
 
     return render_template('comments2.html', comments=comments)
+
+@app.route('/bootstrap_columns/')
+def bootstrap_columns():
+    #Calling the render_template function to return bootstrap_columns.html page to client
+    return render_template('bootstrap_columns.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
